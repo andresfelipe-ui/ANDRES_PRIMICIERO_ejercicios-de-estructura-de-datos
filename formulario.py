ab0458a9b892d9cd711bcb37eebcf54551ef9851
andresfelipe-ui <andresfeliperodriguezprimicier@gmail.com>
2025-07-10 19:18:41 -0500

    Update formulario.py

--- formulario.py
+++ formulario.py
@@ -8,19 +8,22 @@ class calculadora:
         numero1= input("numero 1: ")
         numero2= input("numero 2: ")
         return numero1, numero2
+        
+        
     
     def almacenar_numero(self, numero1, numero2):
         dato_numero = [numero1, numero2]
         print(dato_numero)
         objlista.guardar_numero(dato_numero)
-        objlista.incluir_lista(dato_numero)
+        objlista.guardar_numero(dato_numero)
+        objlista.guardar_numero([1,5])
+        print(objlista.lista_numero)
         
-        print(f"{objlista.lista_numero} . {dato_numero}"))
+        
     
         
 #codigo principal
 
-if __name__ == "__main__":
-    objcalculadora = Calculadora()
-    auxnum1, auxnum2 = objcalculadora.pedir_numero()
-    objcalculadora.almacenar_numero(auxnum1, auxnum2)
+objcalculadora = calculadora()
+auxnum1, auxnum2= objcalculadora.pedir_numero()
+objcalculadora.almacenar_numero(auxnum1, auxnum2)
